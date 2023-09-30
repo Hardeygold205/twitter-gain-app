4810a4b68fe0bd30fa5f720e8a4db3bc62e34a24
Hardeygold205 <surajudeenhadiademola330@gmail.com>
2023-09-30 20:49:35 +0100

    My Personal Portfolio

--- script.js
+++ script.js
@@ -1,15 +1,4 @@
 
-const Dark = document.getElementById("dark");
-const Light = document.getElementById("light");
-const Body = document.body;
-
-Dark.addEventListener('click', () => {
-    Body.classList.toggle("darkmode")
-});
-Light.addEventListener('click', () => {
-    Body.classList.remove("darkmode")
-});
-
 const Hamburger = document.getElementById("menu");
 const SideMenu = document.getElementById("sidebar");
 
